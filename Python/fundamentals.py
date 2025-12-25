e7b79bc72b37d883240064be1b03bb882140e395
Abhishek Kumar Singh <abhisheks02129@gmail.com>
2025-12-25 21:34:34 +0530

    first commit

---
+++ Python/fundamentals.py
@@ -0,0 +1,12 @@
+print("hello world! I am learning AI/ML");
+print("I am learning AI/ML ", "and Todays date is 25th December 2025");
+print("new line print\n I am in new line");
+
+x=10;
+print(type(x)) #<class 'int'>
+
+name="abhishek"
+print(type(name)) #<class 'str'>
+
+ 
+
---
+++ Python/fundamentals_assignment_1.py
@@ -0,0 +1,28 @@
+name=input("enter your name: ");
+age = int(input("Enter your age: "));
+print(f"Hello {name} , you are {age} years old");
+
+
+#######################################################
+
+
+num_1 = int(input("Enter the first number: "));
+num_2 = int(input("Enter the second number: "));
+print(num_1+num_2)
+print(num_1-num_2)
+print(num_1*num_2)
+print(num_1/num_2)
+
+
+#########################################################
+
+
+floatNum=float(input("Enter the float number: "))
+intNum= int(input("Enter the number: "))
+intNum2 = int(input("Enter the second number: "))
+
+average = (floatNum+intNum+intNum2) / 3 ;
+print(f"their avergae is {average}")
+
+##########################################################
+
