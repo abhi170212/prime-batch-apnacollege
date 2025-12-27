0b263b71c3f89310f930f90335ed596de0e9dd72
Abhishek Kumar Singh <abhisheks02129@gmail.com>
2025-12-27 23:17:45 +0530

    new python

---
+++ Python/fundamentals4/objects.py
@@ -0,0 +1,11 @@
+class student:
+     subject="Python"
+     college="ABC"
+     year="4th year"
+
+stu1= student()
+stu2= student()
+
+print("this is the address of the object_1" , stu1)
+print(stu1.subject)
+print(type(stu1));
